a02f9cb96312d76761a66f28bf4d1ecf1c3256af
dingtalk_fissho <3166146424@qq.com>
2021-07-07 11:20:51 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { Button } from 'ant-design-vue';
+import Button from 'ant-design-vue/lib/button';
+import 'ant-design-vue/lib/button/style/css';
 const app = createApp(App)
 app.use(Button)
 app.use(store).use(router).mount('#app')
