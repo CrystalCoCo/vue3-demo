c79fb49e5cb7edacaa159ea2af41e3b57cda861e
dingtalk_fissho <3166146424@qq.com>
2021-07-07 15:44:27 +0800

    国际化配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <a-button type="primary">Primary</a-button>
+    <a-table :dataSource="dataSource" :columns="columns" />
   </div>
+  <a-pagination show-size-changer v-model:current="current1" v-model:pageSize="pageSize" :total="500" @showSizeChange="onShowSizeChange" />
 </template>
 
 <script>
+  import { Table, Pagination } from 'ant-design-vue'
+  
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      ATable: Table,
+      APagination: Pagination
+    },
+    data() {
+      return {
+        dataSource: [
+          {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+          },
+          {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+          },
+        ],
+
+        columns: [
+          {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+          },
+          {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+          },
+          {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+          },
+        ]
+      }
+    }
   }
 </script>
