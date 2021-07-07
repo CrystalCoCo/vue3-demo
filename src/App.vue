17da9158f07d5cb94b13aeb2b8f4f63bb4cad076
dingtalk_fissho <3166146424@qq.com>
2021-07-07 09:54:11 +0800

    全局引入ant-design-vue组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <a-button>Default</a-button>
+    <a-button type="primary">
+      Primary
+    </a-button>
   </div>
   <router-view/>
 </template>
